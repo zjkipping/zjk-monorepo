9e58b6a4d6e5159404df0d4bdf8f470d9b8ae3a4
Zachary Kipping <zjkipping@hotmail.com>
2023-03-20 21:58:39 -0500

    Consume auth token from implicit grant redirect

--- libs/ani-list/data-access-auth/src/lib/ani-list-auth.service.ts
+++ libs/ani-list/data-access-auth/src/lib/ani-list-auth.service.ts
@@ -5,15 +5,27 @@ import { Router } from '@angular/router';
 import { AniListEnvironment } from '@zjk/ani-list/util-environment';
 import { EnvironmentService } from '@zjk/infrastructure/util-environment';
 
+interface ImplicitGrantUrlFragments {
+  access_token: string;
+  token_type: string;
+  expires_in: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class AniListAuthService {
+  public accessToken: string | null = null;
+
   constructor(
     @Inject(DOCUMENT) private document: Document,
     private es: EnvironmentService<AniListEnvironment>,
     private router: Router,
   ) {}
+
+  hasAuth() {
+    return !!this.accessToken;
+  }
 
   login() {
     const clientId = this.es.environment.aniListClientId;
@@ -25,6 +37,22 @@ export class AniListAuthService {
   }
 
   consumeRedirectToken(redirectTo: string) {
+    const implicitGrant: ImplicitGrantUrlFragments = this.document.location.hash
+      .substring(1)
+      .split('&')
+      .reduce((prev, curr) => {
+        const parts = curr.split('=');
+        return {
+          ...prev,
+          [parts[0]]: parts[1],
+        };
+      }, {} as ImplicitGrantUrlFragments);
+
+    this.accessToken = implicitGrant.access_token;
     this.router.navigate([redirectTo]);
+  }
+
+  logout() {
+    this.accessToken = null;
   }
 }

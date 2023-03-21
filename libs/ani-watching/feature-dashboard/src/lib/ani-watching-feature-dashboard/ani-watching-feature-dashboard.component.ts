7e935d9a398fe53db894ba6cfe1dc28255cffaa4
Zachary Kipping <zjkipping@hotmail.com>
2023-03-21 16:56:58 -0500

    Raw dumping data onto screen for now

--- libs/ani-watching/feature-dashboard/src/lib/ani-watching-feature-dashboard/ani-watching-feature-dashboard.component.ts
+++ libs/ani-watching/feature-dashboard/src/lib/ani-watching-feature-dashboard/ani-watching-feature-dashboard.component.ts
@@ -1,5 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import {
+  AniListMediaListService,
+  Media,
+} from '@zjk/ani-list/data-access-media-list';
+import {
+  AniListUserInfo,
+  AniListUserInfoService,
+} from '@zjk/ani-list/data-access-user-info';
 
 @Component({
   selector: 'zjk-ani-watching-feature-dashboard',
@@ -8,4 +18,17 @@ import { Component } from '@angular/core';
   templateUrl: './ani-watching-feature-dashboard.component.html',
   styleUrls: ['./ani-watching-feature-dashboard.component.scss'],
 })
-export class AniWatchingFeatureDashboardComponent {}
+export class AniWatchingFeatureDashboardComponent {
+  userInfo: Observable<AniListUserInfo>;
+  currentlyWatchingList: Observable<Media[]>;
+  planningToWatchList: Observable<Media[]>;
+
+  constructor(
+    userInfoService: AniListUserInfoService,
+    mediaListService: AniListMediaListService,
+  ) {
+    this.userInfo = userInfoService.userInfo;
+    this.currentlyWatchingList = mediaListService.currentlyWatching;
+    this.planningToWatchList = mediaListService.planningToWatch;
+  }
+}

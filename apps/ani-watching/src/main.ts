318e20a8d9fbd6abbf7c2b037bd6d5b836b4da10
Zachary Kipping <zjkipping@hotmail.com>
2023-03-20 20:12:07 -0500

    Tweaks on angular app generate

--- apps/ani-watching/src/main.ts
+++ apps/ani-watching/src/main.ts
@@ -1,7 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
-import { AppComponent } from './app/app.component';
-import { appRoutes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
